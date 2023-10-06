8dd5027c4557ff835b92cb6073ca91761f0f5f90
noob_bokhonto <bsbickysharma3@gmail.com>
2023-10-07 04:41:10 +0530

    key removed

--- Book My Show/config/db.js
+++ Book My Show/config/db.js
@@ -1,5 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://bsbickysharma3:BsLeeaMt5avZfnnq@bokhontoz-cluster.ndf6zs3.mongodb.net/?retryWrites=true&w=majority";
+const uri = "SAMPLE_URI"; // STORE THE URI IN GITHUB SECRETS
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
